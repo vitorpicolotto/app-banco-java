098c4da42bfda22133af059ea5a89eef96ebc7e0
vitorpicolotto <picolottovitor@gmail.com>
2025-03-13 23:05:21 -0300

    Criando o menu e o loop inicial

--- src/Banco.java
+++ src/Banco.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Banco {
     public static void main(String[] args) {
         //Entrando com as informações do cliente
@@ -10,6 +12,26 @@ public class Banco {
         System.out.println("Tipo da conta: " + tipoConta);
         System.out.println("Saldo atual: R$" + saldo);
         System.out.println("\n******************");
+
+        //Criação do menu interativo de opções
+        int opcao = 0;
+
+        String menu = """
+                \n*** Digite a opção que você deseja acessar ***
+                1 - Consultar Saldo
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                \n Opção:
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            System.out.println("Você escolheu a opção " + opcao);
+        }
 
     }
 }

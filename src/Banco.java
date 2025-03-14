3f13ef3da8d8d0a95de75e23bd6dde9676cbf675
vitorpicolotto <picolottovitor@gmail.com>
2025-03-13 22:52:50 -0300

    Primeiro commit: criação do projeto e entrada de dados do cliente

---
+++ src/Banco.java
@@ -0,0 +1,15 @@
+public class Banco {
+    public static void main(String[] args) {
+        //Entrando com as informações do cliente
+        String nome = "Vitor";
+        String tipoConta = "Conta Corrente";
+        double saldo = 5000.00;
+
+        System.out.println("******************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo atual: R$" + saldo);
+        System.out.println("\n******************");
+
+    }
+}
